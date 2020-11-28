a305bf1b80f4fe0b30db54523dce439836235f62
Anastasiia Sai <natya.sai@yandex.ru>
2020-11-28 12:57:17 +0300

    исправлены сбросы инпутов

--- scripts/validate.js
+++ scripts/validate.js
@@ -19,9 +19,10 @@ function checkInputValidity(form, input, config) {
 }
 
 function setButtonState(button, isActive, config) {
+
     if (isActive) {
         button.classList.remove(config.buttonInvalidClass);
-        button.disabled = false;
+        button.disabled = false;    
     } else {
         button.classList.add(config.buttonInvalidClass);
         button.disabled = true; 
@@ -47,7 +48,6 @@ function enableValidation(config) {
 
         form.addEventListener('submit', (evt) => {
             evt.preventDefault();
-            console.log('отправка формы');
         });
 
         const submitButton = form.querySelector(config.submitButtonSelector);
@@ -61,6 +61,7 @@ const validationConfig = {
     submitButtonSelector: '.popup__input-buttom',
     buttonInvalidClass:'popup__input-buttom_invalid',
     inputInvalidClass: 'popup__input-invalid',
+    errorMessage: '.error',
 };
 
 enableValidation(validationConfig);
